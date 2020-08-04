10beb5d14763c6a211f097ae0852f9e311782e07
Hiroshi Suda <suda@net.it-chiba.ac.jp>
2020-08-04 21:18:30 +0900

    デザインを前回授業に合わせた

--- montecarlo/montecarlo.js
+++ montecarlo/montecarlo.js
@@ -1,3 +1,4 @@
+"use strict";
 window.addEventListener("load", () => {
   var mui = new Montecarlo_ui(
     document.querySelector("#start"),
@@ -5,15 +6,19 @@ window.addEventListener("load", () => {
     document.querySelector("#graph1"),
     document.querySelector("#graph1b")
   );
-  mui.setNumber(document.querySelectorAll("#panel2 > button"));
-  mui.setResult(document.querySelector("#result"));
+  mui.setNumber_ui(document.querySelectorAll("#panel2 > button"));
+  mui.setResult_ui(document.querySelector("#answer"));
+  mui.draw_axis();
 });
 
 class Montecarlo_ui {
   constructor(start, clear, canvas, background) {
     this.start = start;
     this.clear = clear;
+    this.canvas = canvas;
     this.montecarlo = new Montecarlo(canvas);
+    this.background = background;
+
     this.clear.addEventListener("click", () => {
       this.montecarlo.clearCanvas();
     });
@@ -21,13 +26,13 @@ class Montecarlo_ui {
       let result = this.montecarlo.next();
       this.result.textContent =
         "計算結果：" +
-        result[0] / result[1] +
+        result[0] / result[1] * 4.0 +
         "(試行回数：" +
         result[1] +
         "回)";
     });
   }
-  setNumber(btnArray) {
+  setNumber_ui(btnArray) {
     for (let btn of btnArray) {
       btn.addEventListener("click", () => {
         if (btn.getAttribute("number"))
@@ -35,21 +40,33 @@ class Montecarlo_ui {
       });
     }
   }
-  setResult(result_element) {
+  setResult_ui(result_element) {
     this.result = result_element;
-    this.result.textContent = "計算結果：";
+    this.result.textContent = "計算値：";
+  }
+  draw_axis() {
+    console.log(this.background);
+    let ctx2 = this.background.getContext('2d');
+    ctx2.beginPath();
+    ctx2.moveTo(0,this.canvas.height/2);
+    ctx2.lineTo(this.canvas.width,this.canvas.height/2);
+    ctx2.moveTo(this.canvas.width/2,0);
+    ctx2.lineTo(this.canvas.width/2,this.canvas.height);
+    ctx2.stroke();
   }
 }
 class Montecarlo {
   constructor(canvas) {
     this.canvas = canvas;
     this.ctx = canvas.getContext("2d");
-    this.times = 10000;
-    this.all_number = 0;
-    this.inside_number = 0;
+    this.times = 10000; // startボタンが押された際の打点数
+    this.all_number = 0;  // 全打点数（累積)
+    this.inside_number = 0; // 円内に入った点数(累積)
   }
   next() {
     for (let i = 0; i < this.times; i++) {
+      // Math.randomの返り値は0.0〜1.0
+      // これを-1.0〜1.0に整える
       let x = Math.random() * 2.0 - 1.0;
       let y = Math.random() * 2.0 - 1.0;
 
@@ -63,8 +80,9 @@ class Montecarlo {
       this.all_number++;
 
       this.ctx.beginPath();
-      let px = (x + 1.0) * 200.0;
-      let py = (y + 1.0) * 200.0;
+      // -1.0〜1.0の座標を0〜横幅（または高さ）に整える
+      let px = (x + 1.0) * this.canvas.width / 2.0;
+      let py = (y + 1.0) * this.canvas.height / 2.0;
       this.ctx.moveTo(px, py);
       this.ctx.lineTo(px + 1, py + 1);
       this.ctx.stroke();
@@ -72,7 +90,7 @@ class Montecarlo {
     return [this.inside_number, this.all_number];
   }
   clearCanvas() {
-    this.ctx.clearRect(0, 0, 400, 400);
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
     this.inside_number = 0;
     this.all_number = 0;
   }
